55216bfee858160d9410b24965fdfc67715e1a27
Goran <goran@crawlyfi.com>
2019-03-29 22:53:00 +0100

    added event service

---
+++ src/main/java/com/crawlyfi/lib/service/Event.java
@@ -0,0 +1,99 @@
+package com.crawlyfi.lib.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.crawlyfi.lib.pojo.Message;
+import com.crawlyfi.lib.pojo.Source;
+import com.crawlyfi.lib.pojo.Variable;
+import com.crawlyfi.lib.message_broker.Producer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Event {
+
+    private Message message;
+    private Event parentEvent;     
+    private Producer producer;
+    private String payload;
+
+    public Event(Producer producer, String configId, String crawlerId, ArrayList<Variable> variableList, ArrayList<Source> sourceList, Integer rowId) {        
+        setProducer(producer);        
+        setMessage(new Message());
+        getMessage().setConfigId(configId);
+        getMessage().setCrawlerId(crawlerId);
+        getMessage().setRowId(rowId);
+        getMessage().setVariableList(variableList);
+        getMessage().setSourceList(sourceList);        
+        publish();
+    }
+  
+	public void publish() {
+		Gson gson = new GsonBuilder().disableHtmlEscaping().create();        
+        setPayload(gson.toJson(message));
+        System.out.println(getPayload());
+        try {
+            producer.sendMessage(getPayload());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    };
+
+    /**
+     * @return the parentEvent
+     */
+    public Event getParentEvent() {
+        return parentEvent;
+    }
+
+    /**
+     * @param parentEvent the parentEvent to set
+     */
+    public void setParentEvent(Event parentEvent) {
+        this.parentEvent = parentEvent;
+    }
+
+
+    /**
+     * @return the producer
+     */
+    public Producer getProducer() {
+        return producer;
+    }
+
+    /**
+     * @param producer the producer to set
+     */
+    public void setProducer(Producer producer) {
+        this.producer = producer;
+    }
+
+    /**
+     * @return the message
+     */
+    public Message getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+
+    /**
+     * @return the payload
+     */
+    public String getPayload() {
+        return payload;
+    }
+
+    /**
+     * @param payload the payload to set
+     */
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+
+}
