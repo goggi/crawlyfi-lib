9b87493b34932198a8bfbe7f4a0d171adfbd6d6b
Goran <goran@crawlyfi.com>
2019-04-03 16:24:29 +0200

    added factory

---
+++ src/main/java/com/crawlyfi/lib/service/Factory.java
@@ -0,0 +1,27 @@
+package com.crawlyfi.lib.service;
+
+import com.crawlyfi.lib.pojo.Message;
+import com.google.gson.Gson;
+
+public class Factory {
+	
+	private Message message;
+
+	public Factory(String payload){
+		setMessage(new Gson().fromJson(payload, Message.class));        
+	}
+
+	/**
+	 * @return the message
+	 */
+	public Message getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(Message message) {
+        setMessage(message);        
+	}
+}
