4d4cb094898f06ba30eebc7e13ca9b70b1a6a8e4
Goran <goran@crawlyfi.com>
2019-04-05 10:46:16 +0200

    added producerList to Factory

--- src/main/java/com/crawlyfi/lib/service/Factory.java
+++ src/main/java/com/crawlyfi/lib/service/Factory.java
@@ -1,11 +1,15 @@
 package com.crawlyfi.lib.service;
 
+import java.util.HashMap;
+
+import com.crawlyfi.lib.message_broker.Producer;
 import com.crawlyfi.lib.pojo.Message;
 import com.google.gson.Gson;
 
 public class Factory {
 	
 	private Message message;
+	private HashMap<String, Producer> producerList;
 
 	public Factory(String payload){
 		setMessage(new Gson().fromJson(payload, Message.class));        
@@ -23,5 +27,19 @@ public class Factory {
 	 */
 	public void setMessage(Message message) {
         this.message = message;        
+	}
+
+	/**
+	 * @return the producerList
+	 */
+	public HashMap<String, Producer> getProducerList() {
+		return producerList;
+	}
+
+	/**
+	 * @param producerList the producerList to set
+	 */
+	public void setProducerList(HashMap<String, Producer> producerList) {
+		this.producerList = producerList;
 	}
 }
